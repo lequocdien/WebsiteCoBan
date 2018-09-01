63c5cd0c61112fc7b5aeb43ac837e641c52c758d
dql1998 <dql1998@gmail.com>
2018-09-01 08:22:08 +0700

    commit lan 1

---
+++ js/1.js
@@ -0,0 +1,38 @@
+﻿$(function(){
+	// if($(window).width() < 768)
+	// {
+	// 	$(".navbar-rules").addClass("navbar-rules-collapse");
+	// 	$(".navbar-rules a").click(function(){
+	// 		$(".navbar-rules button").click();
+	// 	});
+	// }
+	// else{
+	$(window).scroll(function(){
+		if($("html,body").scrollTop() > 100)
+		{
+			$(".navbar-rules").addClass("navbar-rules-collapse");
+		}
+		else{
+			$(".navbar-rules").removeClass("navbar-rules-collapse");
+		}
+	});
+	// }
+	$("body").scrollspy({
+		target: ".navbar-rules",
+		offset: 51
+	});
+	$(".navbar-rules a").click(function(){
+		var a = $(this).attr("href");
+		$("html,body").stop().animate({
+			scrollTop: $(a).offset().top - 50},
+			1500,
+			"easeInOutExpo"
+		);
+		return false;
+	});
+	$(".nav_collapse-hide").click(function(){
+		$(".navbar-collapse").collapse("hide");
+	})
+	
+
+});
